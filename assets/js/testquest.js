812f2f1914b256982465623fa0b9e3a09acaab21
Peter Lauer <lauerpeter11@gmail.com>
2023-07-12 17:32:44 -0500

    built question template

---
+++ assets/js/testquest.js
@@ -0,0 +1,29 @@
+
+
+var questions = [
+  {
+    title: "Which of the following is not javascript data types?",
+    choices: ["Null type", "Undefined type", "Number type", "All of the mentioned"],
+    answer: "Null type"
+  },
+  {
+    title: "",
+    choices: ["","","",""],
+    answer: ""
+  },
+  {
+    title: "",
+    choice: ["", "", "", ""],
+    answer: ""
+  },
+  {
+    title: "",
+    choice: ["", "", "", ""],
+    answer: ""
+  },
+  {
+    title:"",
+    choice: ["", "", "", ""],
+    answer: ""
+  }
+];
